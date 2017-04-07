83e9ee16bfde1e8c91e763420224c2ebecb9034b
Colin Hill <colin@colinwhill.com>
2017-04-07 15:05:48 +0100

    [add] implemented basic waypoint/tour functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,24 @@ export class AppComponent {
     lat: number = 51.673858;
     lng: number = 7.815982;
 
+    isRoute: boolean = false;
+    isEditing: boolean = false;
+    isSentToDriver: boolean = false;
+
     clickedMarker(label: string, index: number) {
       console.log(`clicked the marker: ${label || index}`)
     }
 
     mapClicked($event: any) {
+      if (this.isRoute) {
+        console.log(`this is a route point ${this.tours[0].points}`);
+
+        this.tours[0].points.push({
+          lat: $event.coords.lat,
+          lng: $event.coords.lng,
+          draggable: true,
+        })
+      }
       this.markers.push({
         lat: $event.coords.lat,
         lng: $event.coords.lng,
@@ -27,9 +40,69 @@ export class AppComponent {
       });
     }
 
+    // Create
+    createNewTour() {
+      this.isRoute = !this.isRoute;
+      console.log(this.isRoute)
+    }
+
+
     markerDragEnd(m: marker, $event: MouseEvent) {
       console.log('dragEnd', m, $event);
     }
+
+    sendTourToDriver(){
+      this.isSentToDriver =! this.isSentToDriver;
+    }
+
+    editMarkers() {
+      this.isEditing = !this.isEditing;
+
+    }
+
+    // Delete
+    clearMapData() {
+      this.clearAllMarkers();
+      this.clearAllRoutes();
+    }
+    clearAllMarkers() {
+      this.markers = [];
+    }
+
+    clearAllRoutes() {
+      this.tours[0].points = [];
+    }
+
+    deleteTourPoint(m: marker, i: number) {
+      console.log(`${m} ${i}`);
+      this.tours[0].points.splice(i, 1);
+    }
+
+    deleteMarker(m: marker, i: number) {
+      this.markers.splice(i, 1)
+    }
+
+    // Storage
+    tours: tour[] = [
+      {
+        name: "Tour 1",
+        points: [
+          {
+            lat: 52.123413,
+            lng: 12.123124,
+            label: 'T1',
+            draggable: true
+          },
+          {
+            lat: 50.123413,
+            lng: 11.123124,
+            label: 'T2',
+            draggable: true
+          },
+        ]
+      }
+    ]
+
 
     markers: marker[] = [
   	  {
@@ -51,7 +124,15 @@ export class AppComponent {
   		  draggable: true
   	  }
     ]
+
+    drivers: driver[] = [
+      {
+        name: "John",
+        tour: this.tours[0]
+      }
+    ]
   }
+
   // just an interface for type safety.
   interface marker {
   	lat: number;
@@ -59,7 +140,18 @@ export class AppComponent {
   	label?: string;
   	draggable: boolean;
   }
+  interface tour {
+    name?: string;
+    points?: any;
 
-// export class AppComponent {
-//   title: string = 'Map App';
-// }
+
+    // lat: number;
+  	// lng: number;
+  	// label?: string;
+  	// draggable: boolean;
+  }
+
+  interface driver {
+    name: string;
+    tour?: tour;
+  }
